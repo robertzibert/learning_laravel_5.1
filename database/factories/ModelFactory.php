f1a503123ce8830ac6f07dd4643083a0cc1791d7
Robert Zibert <robert_zibert@hotmail.com>
2015-10-22 15:22:40 -0300

    first manyToMany rsp, and first Factory Seed

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,19 @@
 |
 */
 
+
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'email'          => $faker->email,
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Tag::class, function (Faker\Generator $faker){
+    return [
+        'name' => $faker->unique()->randomElement($array = array ('Programming','Networking','DevOps')) ,
     ];
 });
